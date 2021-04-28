54e481527e4f0ac7421941c7e1d602f636a4c680
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-04-28 10:18:08 +1000

    added gem gon

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 
     console.log("hello")
 
+    console.log(`${gon.variable_name}`)
+
     // $("h1").hide();
 })
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    gon.variable_name = 7
   end
 end
